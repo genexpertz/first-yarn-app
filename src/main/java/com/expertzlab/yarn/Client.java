d17e4993f79f6a8bf4ac7d0f7e94632c36753c94
Gireesh Babu <gireesh.babu@expertzlab.com>
2018-08-29 03:11:48 +0530

    Formatted the client

--- src/main/java/com/expertzlab/yarn/Client.java
+++ src/main/java/com/expertzlab/yarn/Client.java
@@ -54,6 +54,7 @@ public class Client {
 
         System.out.println("Initializing ContainerLaunchContext for ApplicationMaster container");
         ContainerLaunchContext amContainer = Records.newRecord(ContainerLaunchContext.class);
+
         System.out.println("Adding LocalResource");
         LocalResource appMasterJar =Records.newRecord(LocalResource.class);
         FileStatus jarStat = FileSystem.get(conf).getFileStatus(jarPath);
@@ -64,27 +65,25 @@ public class Client {
         appMasterJar.setVisibility(LocalResourceVisibility.PUBLIC);
 
         System.out.println("Setting environment");
-        Map<String, String> appMasterEnv = new HashMap<String,
-                        String>();
+        Map<String, String> appMasterEnv = new HashMap<String, String>();
         for (String c : conf.getStrings(YarnConfiguration.YARN_APPLICATION_CLASSPATH, YarnConfiguration.DEFAULT_YARN_APPLICATION_CLASSPATH)){
             Apps.addToEnvironment(appMasterEnv,ApplicationConstants.Environment.CLASSPATH.name(), c.trim());
         }
         Apps.addToEnvironment(appMasterEnv, ApplicationConstants.Environment.CLASSPATH.name(),
                 ApplicationConstants.Environment.PWD.$() + File.separator + "*");
+
         System.out.println("Setting resource capability");
         Resource capability = Records.newRecord(Resource.class);
         capability.setMemory(256);
         capability.setVirtualCores(1);
 
         System.out.println("Setting command to start ApplicationMaster service");
-                amContainer.setCommands(Collections.singletonList(
-                        "/Library/Java/JavaVirtualMachines/jdk1.8.0_65.jdk/Contents/Home/bin/java"
-                + " -Xmx256M" + " com.expertzlab.yarn.ApplicationMaster"
-                + " " + command + " " + String.valueOf(n) + " 1>"
-                + ApplicationConstants.LOG_DIR_EXPANSION_VAR +
-                "/stdout"
-                + " 2>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR
-                + "/stderr"));
+        amContainer.setCommands(Collections.singletonList(
+                "/Library/Java/JavaVirtualMachines/jdk1.8.0_65.jdk/Contents/Home/bin/java"
+                + " -Xmx256M com.expertzlab.yarn.ApplicationMaster"
+                + " " + command + " " + String.valueOf(n)
+                + " 1>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/stdout"
+                + " 2>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/stderr"));
         amContainer.setLocalResources(Collections.singletonMap(
                 "first-yarn-app.jar", appMasterJar));
         amContainer.setEnvironment(appMasterEnv);
@@ -98,12 +97,11 @@ public class Client {
         appContext.setQueue("default");
 
         ApplicationId appId = appContext.getApplicationId();
+
         System.out.println("Submitting application " + appId);
         yarnClient.submitApplication(appContext);
-        ApplicationReport appReport =
-                yarnClient.getApplicationReport(appId);
-        YarnApplicationState appState =
-                appReport.getYarnApplicationState();
+        ApplicationReport appReport = yarnClient.getApplicationReport(appId);
+        YarnApplicationState appState = appReport.getYarnApplicationState();
         while (appState != YarnApplicationState.FINISHED
                 && appState != YarnApplicationState.KILLED
                 && appState != YarnApplicationState.FAILED) {
@@ -111,6 +109,7 @@ public class Client {
             appReport = yarnClient.getApplicationReport(appId);
             appState = appReport.getYarnApplicationState();
         }
+
         if (appState == YarnApplicationState.FINISHED) {
             return true;
         } else {
